ad621e143fe66280ae10b8fbf6f98cca22c77ba4
robertoGarciaEs <rohegares@gmail.com>
2024-03-16 14:12:27 -0500

    Desarrollo del ejercicio 1 de CadenasTexto

---
+++ CadenasTexto/ejercicio1.py
@@ -0,0 +1,10 @@
+cadena = "Te quiero solo como amigo"
+resultado_1 = cadena[0:2]
+print("Los dos caracteres son: ", resultado_1)
+resultado_2 = cadena[22:25]
+print("Los tres caracteres son: ", resultado_2)
+resultado_3 = cadena[0::2]
+print("La cadena cada dos caracteres es: ", resultado_3)
+cadena_invertida = cadena[::-1]
+print("La cadena invertida es: ", cadena_invertida)
+print("La cadena en un sentido y en sentido inverso es: ", cadena + cadena_invertida)
