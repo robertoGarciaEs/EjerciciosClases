466ef13305ba2fb0c2a9125ca13dbdf1c5922f93
robertoGarciaEs <rohegares@gmail.com>
2024-03-16 14:20:17 -0500

    Desarrollo del ejercicio2 de CadenasTexto

--- CadenasTexto/ejercicio2.py
+++ CadenasTexto/ejercicio2.py
@@ -1,3 +1,5 @@
-cadena = "Separado"
-resultado = cadena.replace("",",")
-print(resultado)
+cadena = "Separado y ,"
+resultado_1 = cadena[0:8]
+resultado_2 = resultado_1.replace("",",")
+resultado_3 = resultado_2[1:16]
+print("La nueva cadena es: ", resultado_3)
