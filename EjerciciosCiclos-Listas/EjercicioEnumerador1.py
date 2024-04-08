4db275d1b2f300a893319d2e998585545d3b8cca
robertoGarciaEs <rohegares@gmail.com>
2024-04-08 16:35:55 -0500

    Ejercicio Práctica Enumerador 1

---
+++ EjerciciosCiclos-Listas/EjercicioEnumerador1.py
@@ -0,0 +1,3 @@
+lista_nombres = ["Marcos", "Laura", "Mónica", "Javier", "Celina", "Marta", "Darío", "Emiliano", "Melissa"]
+for indice, nombre in enumerate(lista_nombres):
+    print(nombre ,"se encuentra en el índice", indice)
