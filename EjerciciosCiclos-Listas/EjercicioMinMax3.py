0e2b263e6a6a5859450ca016950a49968207b3c0
robertoGarciaEs <rohegares@gmail.com>
2024-04-08 17:42:28 -0500

    Ejercicio Práctica Min y Max 3

---
+++ EjerciciosCiclos-Listas/EjercicioMinMax3.py
@@ -0,0 +1,5 @@
+diccionario_edades = {"Carlos":55, "María":42, "Mabel":78, "José":44, "Lucas":24, "Rocío":35, "Sebastián":19, "Catalina":2, "Darío":49}
+edad_minima = min(diccionario_edades.values())
+ultimo_nombre = max(diccionario_edades, key=len)
+print("La edad mínima es:", edad_minima)
+print("El último nombre en orden alfabético es:", ultimo_nombre)
