a2a370a82e68a17b128bb4fae52647a1d77736ea
robertoGarciaEs <rohegares@gmail.com>
2024-04-08 17:15:34 -0500

    Ejercicio Práctica Zip 3

---
+++ EjerciciosCiclos-Listas/EjercicioZip3.py
@@ -0,0 +1,5 @@
+lista_1 = ["uno", "dos", "tres", "cuatro", "cinco"]
+lista_2 = ["um", "dois", "três", "quatro", "cinco"]
+lista_3 = ["one", "two", "three", "four", "five"]
+numeros = zip(lista_1, lista_2, lista_3)
+print(list(numeros))
